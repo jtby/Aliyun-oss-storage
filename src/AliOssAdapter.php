541a59c824e1bd24e21433ee48c42bd09babb0cf
jtby <136969236+jtby@users.noreply.github.com>
2025-08-16 15:24:27 +0800

    geturl增加path_prefix

--- src/AliOssAdapter.php
+++ src/AliOssAdapter.php
@@ -568,7 +568,7 @@ class AliOssAdapter extends AbstractAdapter
      */
     public function getUrl( $path )
     {
-        return ( $this->ssl ? 'https://' : 'http://' ) . ( $this->isCname ? ( $this->cdnDomain == '' ? $this->endPoint : $this->cdnDomain ) : $this->bucket . '.' . $this->endPoint ) . '/' . ltrim($path, '/');
+        return ( $this->ssl ? 'https://' : 'http://' ) . ( $this->isCname ? ( $this->cdnDomain == '' ? $this->endPoint : $this->cdnDomain ) : $this->bucket . '.' . $this->endPoint ) . '/' . $this->applyPathPrefix($path);
     }
 
     /**
